6b84578a2bf7845b743f35dea0077a12abb1d7da
ctlaptop <tubin478@gmail.com>
2025-06-20 21:48:54 -0600

    se creo metodo para conectar a la base de datos

--- ToDoList/src/Modelo/TareaDataBase.java
+++ ToDoList/src/Modelo/TareaDataBase.java
@@ -4,10 +4,26 @@
  */
 package Modelo;
 
-/**
- *
- * @author GP
- */
+import java.sql.*;
+
+
 public class TareaDataBase {
-    
+
+    Connection conexion = null;
+    String database = "tareas";
+    String url = "jdbc:mysql://localhost:3306/" + database;
+    String usuario = "root";
+    String clave = "020592";
+
+    public Connection obtenerConexion() {
+        try {
+            conexion = DriverManager.getConnection(url, usuario, clave);
+
+        } catch (SQLException e) {
+            System.out.println("Ocurrio un erro al conectarse a la base de datos " + database);
+            System.out.println(e);
+        } 
+
+        return conexion;
+    }
 }
