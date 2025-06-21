a309fb9115e62cf74cb2058517abd4db81b8edca
ctlaptop <tubin478@gmail.com>
2025-06-20 23:16:03 -0600

    se realizo un extends de TareaDataBase a TareaTools

--- ToDoList/src/Modelo/Tarea.java
+++ ToDoList/src/Modelo/Tarea.java
@@ -3,11 +3,65 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Modelo;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-/**
- *
- * @author GP
- */
 public class Tarea {
-    
+
+    private int id;
+    private String titulo;
+    private String descripcion;
+    private boolean completada;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaCompletacion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void setCompletada(boolean completada) {
+        this.completada = completada;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public LocalDateTime getFechaCompletacion() {
+        return fechaCompletacion;
+    }
+
+    public void setFechaCompletacion(LocalDateTime fechaCompletacion) {
+        this.fechaCompletacion = fechaCompletacion;
+    }
+
+
 }
--- ToDoList/src/Modelo/TareaTools.java
+++ ToDoList/src/Modelo/TareaTools.java
@@ -3,11 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Modelo;
+import java.sql.*;
+public class TareaTools extends TareaDataBase {
+   public boolean registrar(Tarea t){
+       String consulta = "INSERT INTO tareas (titulo,descripcion,completada,fecha_creacion,fecha_completacion) VALUES (?,?,?,?,?)";
+       Connection conexion = obtenerConexion();
 
-/**
- *
- * @author GP
- */
-public class TareaTools {
-    
+   return true;
+   } 
 }
