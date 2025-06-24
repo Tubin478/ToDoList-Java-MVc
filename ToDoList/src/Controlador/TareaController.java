68a5e74a07662a462eea5e6f7a300b27e1440843
dvlsqz <danielvel1977@gmail.com>
2025-06-23 18:37:22 -0600

    implementacion verificar estado
    se implemento la verificacion del estado de la tarea

--- ToDoList/src/Controlador/TareaController.java
+++ ToDoList/src/Controlador/TareaController.java
@@ -49,6 +49,11 @@ public class TareaController implements ActionListener {
         }
 
     }
+
+    public void verificarEstado(){
+
+        return "modulo para verificar un estado de tarea";
+    }
     
     //prueba comentario para commit sencillo
     
